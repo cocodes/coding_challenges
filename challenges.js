6b839f57b06054663635e1c98b84d0a63c23a8d1
Johnathanblackburncodes <jhblackburn90@gmail.com>
2021-04-05 10:42:58 -0700

    added another example of triple add function

--- challenges.js
+++ challenges.js
@@ -17,3 +17,15 @@ tripleAdd(10)(20)(30);
 // returns total of all 3 numbers added together
 tripleAdd1(10, 20, 30);
 // returns total of all 3 numbers added together
+
+function quadrupleAdd(num1) {
+  return function (num2) {
+    return function (num3, num4) {
+      return num1 + num2 + num3 + num4;
+    };
+  };
+}
+
+quadrupleAdd(10)(20)(30, 40);
+
+// Day 2 -
