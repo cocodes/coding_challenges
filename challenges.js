0a6fb214c22749e4585f35c703e7649abb654e05
Johnathanblackburncodes <jhblackburn90@gmail.com>
2021-04-06 09:09:57 -0700

    another example

--- challenges.js
+++ challenges.js
@@ -36,3 +36,15 @@ quadrupleAdd(10)(20)(30, 40);
 (function doubleNumber(num) {
   return num * 2;
 })(10);
+
+(function () {
+  function getTotal(a, b) {
+    return a + b;
+  }
+
+  var $ = "currency";
+
+  if (true) console.log("hello world");
+})();
+
+// Day 3 -
