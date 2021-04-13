103980ada973763bc61e2f43528320c9168851e1
Johnathanblackburncodes <jhblackburn90@gmail.com>
2021-04-13 13:13:19 -0700

    start of day 7

--- challenges.py
+++ challenges.py
@@ -54,3 +54,10 @@ class Solution:
             ans = max(ans,right-left+1)
             right+=1
         return ans
+
+# Day 7 - Find first and last position of Element in Sorted Array
+#Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+#If target is not found in the array, return [-1, -1].
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
