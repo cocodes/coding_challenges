7c68b984440b60ccf3d2992744e01109a8e4f603
Johnathanblackburncodes <jhblackburn90@gmail.com>
2021-04-15 11:28:14 -0700

    start of day 9

--- challenges.py
+++ challenges.py
@@ -73,3 +73,11 @@ class Solution:
         intendedSum = n*(n+1)/2
 
         return int(intendedSum-currentSum)
+
+
+# Day 9 - Count Primes
+# Count the number of prime numbers less than a non-negative number, n.
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        
