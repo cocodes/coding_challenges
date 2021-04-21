bb2f7adb7290c8d880a6478e06f35b9a711b19d0
Johnathanblackburncodes <jhblackburn90@gmail.com>
2021-04-21 09:52:48 -0700

    start of day 13

--- challenges.py
+++ challenges.py
@@ -121,3 +121,32 @@ class Solution:
                 profit = max(profit, price-buyPrice)
 
         return profit
+
+# Day 13 - Min Stack
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        
+
+    def push(self, val: int) -> None:
+        
+
+    def pop(self) -> None:
+        
+
+    def top(self) -> int:
+        
+
+    def getMin(self) -> int:
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
